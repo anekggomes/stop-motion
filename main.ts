bbbe9f42327faa836ead5daae20976430c8a1607
anekggomes <00001098808472sp@al.educacao.sp.gov.br>
2025-04-01 08:35:14 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,73 @@
+basic.forever(function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . # . . .
+        # # # # .
+        # # . . .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # .
+        . . # . #
+        . . . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . .
+        . # # # #
+        . . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # # .
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        # # # # .
+        . . # . .
+        . # . # .
+        # . . # .
+        `)
+})
